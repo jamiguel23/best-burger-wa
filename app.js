0c80f2389bb4b3954b105d664c34be0ba8be3461
jamiguel23 <josematthewmiguel@gmail.com>
2020-12-12 16:53:40 -0800

    added else if ofr onion rings and else for other

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ console.log("Hello, World!"); // "console echos Hello, World!""
 var firstName; //creating varible firstName     
 firstName="Matt"; // firstName valued as Matt
 console.log (firstName); 
+
+var promtQuestion = "Do you prefer a fries or onion rings?";
+var response = prompt(promtQuestion);
+console.log (response);
+if(response === "fries") {
+    console.log("Awesome! We prefer onion rings in this house")
+
+}  else if(response==="onion rings"){
+    console.log("Dope! We prefer fries in this house");
+
+} else {
+    console.log(" Gross. Have you tried fries or onion ring?")
+}
