fff29dba07ca33168288d1c253ea46301d952e73
jamiguel23 <josematthewmiguel@gmail.com>
2020-12-15 22:15:26 -0800

    added password promt and cleaned up whitespace

--- app.js
+++ app.js
@@ -7,47 +7,52 @@
 
 var response;
 
+var alertMessage; // declare varible 'alertMessage'
+var color; // declare variable color'
+var promtQuestion = "Do you prefer a fries or onion rings?"; //delare 'promtQuestion' for the start up question
 
-function getUserPromt() { // create a message cariable to be used as argument 
-    var promtQuestion = "Do you prefer a fries or onion rings?"; //delare 'promtQuestion' for the start up question
+function validateUserPassword() {
+    var storedPassword = "Yo";
+    var userPassword =  "";
+
+    while(userPassword !== storedPassword) {
+        userPassword= prompt('input password');
+    }
+}
+
+function getUserPromt() { // create a message cariable to be used as argument  
     var response = prompt(promtQuestion); // declared variable 'response' to write promtQuuestion
-    response = response.toUpperCase();
+    response = response.toLowerCase();
+    return response;
     console.log (response); // logged response in console for later use
 }
 
-var alertMessage; // declare varible 'alertMessage'
-var color; // declare variable color' 
-
 function reloadWittyMessage (response) { // decalred function 'reloadWittyMessage'
     if(response === "fries") { //expected response 
-    alertMessage = "Awesome! We prefer onion rings in this house"; // alert message 
-    color = "#FFFF00"; //assigned 'fries a color
-
-}  else if(response==="onion rings"){ //created else statement to if response is 'onion rings'
-    alertMessage = "Dope! We prefer fries in this house"; //
-    color = "#FFFFFF";
-
-} else {
-    alertMessage = " Gross. Have you tried fries or onion rings?"
-    color = "#FF0000";
+        alertMessage = "Awesome! We prefer onion rings in this house"; // alert message 
+        color = "#FFFF00"; //assigned 'fries a color
+    } else if(response==="onion rings"){ //created else statement to if response is 'onion rings'
+        alertMessage = "Dope! We prefer fries in this house"; //
+        color = "#FFFFFF";
+    } else {
+        alertMessage = " Gross. Have you tried fries or onion rings?"
+        color = "#FF0000";
+    }
 
     var element = document.getElementById('response-output')
     element.innerText = alertMessage;
     element.style.color = color;
-} }
-
-getUserPromt ();
-reloadWittyMessage (response);
-alert(alertMessage);
-
+}
 
 function counter() { 
     for ( var i = 0; i<10; i++){
     console.log(i);
-    
-}}
+    }
+}
 
-
+validateUserPassword ()
+var fries=getUserPromt();
+reloadWittyMessage (fries);
+alert(alertMessage);
 counter ()
-
 
